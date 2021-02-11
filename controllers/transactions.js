2aa93462cd4b7f6d48535fa319b684ec1a6502d0
JooYoo <jooyoo@outlook.com>
2021-02-11 12:14:17 +0100

    04_basic_controller

---
+++ controllers/transactions.js
@@ -0,0 +1,26 @@
+// @desc   get all transactions
+// @route  GET /api/v1/transactions
+// @access Public
+const getTransactions = (req, res, next) => {
+  res.send('GET transactions');
+};
+
+// @desc   add transactions
+// @route  POST /api/v1/transactions
+// @access Public
+const addTransactions = (req, res, next) => {
+  res.send('POST transactions');
+};
+
+// @desc   delete transactions
+// @route  DELETE /api/v1/transactions/:id
+// @access Public
+const deleteTransaction = (req, res, next) => {
+  res.send('DELETE transactions');
+};
+
+module.exports = {
+  getTransactions,
+  addTransactions,
+  deleteTransaction,
+};
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-// the router of the app will get response
-router.get('/', (req, res) => res.send('Hello router'));
+// get route-controllers
+const {
+  getTransactions,
+  addTransactions,
+  deleteTransaction,
+} = require('../controllers/transactions');
+
+// init routes via controller
+router.route('/').get(getTransactions).post(addTransactions);
+
+router.route('/:id').delete(deleteTransaction);
 
 module.exports = router;
