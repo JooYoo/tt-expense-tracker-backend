a819698d4130f3ae74ce3ea59aee52e67a4ac1cd
JooYoo <jooyoo@outlook.com>
2021-02-13 12:21:33 +0100

    08_controller_get_data_from_db

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,8 +1,23 @@
+const Transaction = require('../models/Transaction');
+
 // @desc   get all transactions
 // @route  GET /api/v1/transactions
 // @access Public
-const getTransactions = (req, res, next) => {
-  res.send('GET transactions');
+const getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
 };
 
 // @desc   add transactions
