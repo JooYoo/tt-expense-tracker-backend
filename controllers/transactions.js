db8b707222293c4fd0d3f57267dd4eacdd7b9c0a
JooYoo <jooyoo@outlook.com>
2021-02-13 16:38:23 +0100

    10_controller_delete_data_from_db

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -55,8 +55,33 @@ const addTransaction = async (req, res, next) => {
 // @desc   delete transactions
 // @route  DELETE /api/v1/transactions/:id
 // @access Public
-const deleteTransaction = (req, res, next) => {
-  res.send('DELETE transactions');
+const deleteTransaction = async (req, res, next) => {
+  try {
+    // get the Document by id from URL
+    const transaction = await Transaction.findById(req.params.id);
+
+    // can not found the Document
+    if (!transaction) {
+      return res.status(404).json({
+        success: false,
+        error: 'No transaction found',
+      });
+    }
+
+    // remove the Document
+    await transaction.remove();
+
+    // after remove response
+    return res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
 };
 
 module.exports = {
